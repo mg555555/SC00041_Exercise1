bf85f7d0541ad7928459bec3a944aa0d256b73db
BernadetteMayer <bernadette.m.mayer@gmail.com>
2025-05-28 11:45:03 +0200

    adds readme and new .py file

--- analyze_text.py
+++ analyze_text.py
@@ -1,19 +1,12 @@
-My_file = "/home/user/Desktop/sample.txt" 
+my_file = "/home/user/Desktop/sample.txt"
 
 print("Line\tChars\tUppercase\t% Upper")
 
-f = open(My_file)
-i = 1
-for l in f:
-    total = len(l.strip())
-    upper = 0
-    for c in l:
-        if c.isupper():
-            upper += 1
-    if total != 0:
-        p = upper / total * 100
-    else:
-        p = 0
-    print(str(i) + "\t" + str(total) + "\t" + str(upper) + "\t" + str(round(p, 2)) + "%")
-    i += 1
+with open(my_file, 'r') as f:
+    for i, line in enumerate(f, start=1):
+        stripped_line = line.strip()
+        total = len(stripped_line)
+        upper = sum(1 for c in stripped_line if c.isupper())
+        p = (upper / total * 100) if total != 0 else 0
+        print(f"{i}\t{total}\t{upper}\t{round(p, 2)}%")
 
